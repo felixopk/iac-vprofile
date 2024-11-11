46dcd13c943b1cb3ecbb3e76d14e8b23a010631e
felixopk <felixoppong2000@yahoo.com>
2024-11-11 20:23:14 +0100

    fixed #!

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.30.0"   # Latest AWS provider version as of now
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6.0"    # Latest random provider version as of now
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.4"    # This is already the latest version
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3.2"    # This is already the latest version
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.26.0"   # Latest Kubernetes provider version as of now
     }
   }
 
@@ -32,8 +32,5 @@ terraform {
     region = "eu-north-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.6.6"  # Latest Terraform version as of now
 }
-##
-##
-##
